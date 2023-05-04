11b7c2f35d90524013cb33f5c3992561e8695536
jooohyunpark <jhp527@nyu.edu>
2023-05-03 21:52:39 -0400

    test px unit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ const App = () => {
   return (
     <div>
       {/* https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/sizes */}
+
+      <h2>vw unit</h2>
       <img
         srcSet="https://picsum.photos/300 300w, https://picsum.photos/600 600w, https://picsum.photos/900 900w, https://picsum.photos/1200 1200w"
         sizes="(min-width: 1000px) 25vw, (min-width: 500px) 50vw, 100vw"
@@ -12,6 +14,7 @@ const App = () => {
 
       <hr />
 
+      <h2>px unit</h2>
       <div style={{ width: 800, background: 'orange' }}>
         <img
           srcSet="https://picsum.photos/300 300w, https://picsum.photos/600 600w, https://picsum.photos/900 900w, https://picsum.photos/1200 1200w"
